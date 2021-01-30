e803e45ba749ac530238a036b154a71c57056820
igres9014 <72890882+igres9014@users.noreply.github.com>
2021-01-30 20:20:09 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 30 20:15:11 2021
+
+@author: JOSEPMARIA
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'MyProject',
+    version = '0.1.0',
+    url = '',
+    description = '',
+    packages = find_packages(),
+    install_requires = [
+        # Github Private Repository
+        #'ExampleRepo @ git+ssh://git@github.com/example_organization/ExampleRepo.git#egg=ExampleRepo-0.1'
+        'git+https://github.com/igres9014/gym_chess2.git'
+    ]
+)
